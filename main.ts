94676c9b188baa8c285d005b51e57e91ee5b1357
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-14 13:48:56 -0400

    added ghost mode

--- main.ts
+++ main.ts
@@ -42,6 +42,7 @@ head = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 head.setPosition(120, 60)
+head.setFlag(SpriteFlag.Ghost, true)
 game.onUpdate(function () {
     mySprite.x += controller.dx()
 })
