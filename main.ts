3052aaae0e85e7f5ed27b6b8dbcd0dfe1480a124
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-14 13:55:07 -0400

    task 2 updates

--- main.ts
+++ main.ts
@@ -43,6 +43,27 @@ head = sprites.create(img`
     `, SpriteKind.Player)
 head.setPosition(120, 60)
 head.setFlag(SpriteFlag.Ghost, true)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 3 3 3 3 . . . . . . . . 
+    . . . . 3 . . 3 3 . . . . . . . 
+    . . . . 3 . . . . 3 . . . . . . 
+    . . . . . 3 . . . 3 . . . . . . 
+    . . . . 3 . . . . . 3 . . . . . 
+    . . . . 3 . . . . . 3 . . . . . 
+    . . . . 3 . . . . . 3 3 . . . . 
+    . . . . . 3 . . . . . 3 . . . . 
+    . . . . . . 3 . . . . 3 . . . . 
+    . . . . . . . 3 3 3 3 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite2.setFlag(SpriteFlag.Ghost, false)
+mySprite2.setPosition(30, 90)
 game.onUpdate(function () {
     mySprite.x += controller.dx()
+    mySprite.y += controller.dy()
 })
